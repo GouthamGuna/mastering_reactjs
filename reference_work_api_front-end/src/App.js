9a2384d27cb36c16171439745d054a7dd346f890
GouthamGuna <ggowthamsankar@gmail.com>
2023-12-20 08:37:53 +0530

    Updat App.js

--- reference_work_api_front-end/src/App.js
+++ reference_work_api_front-end/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import StaffTimeTable from './components/StaffTimeTable';
+import "./App.css";
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StaffTimeTable from "./components/StaffTimeTable";
+import TeacherClassTeacher from "./components/TeacherClassTeacher";
 
 function App() {
   return (
     <div className="App">
-      <StaffTimeTable />
+      <h1>Hello Lunar!</h1>
+      <br />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TeacherClassTeacher />} />
+          <Route path="/staffTimeSheets" element={<StaffTimeTable />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
