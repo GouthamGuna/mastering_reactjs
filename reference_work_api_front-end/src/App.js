156e11b84470ba0328c4ed81a7989e822514583b
GouthamGuna <ggowthamsankar@gmail.com>
2023-12-26 14:27:36 +0530

    Update App.js

--- reference_work_api_front-end/src/App.js
+++ reference_work_api_front-end/src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import StaffTimeTable from "./components/StaffTimeTable";
 import TeacherClassTeacher from "./components/TeacherClassTeacher";
+import FileDataReader from "./components/FileDataReader";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Lunar!</h1>
-      <br />
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<TeacherClassTeacher />} />
           <Route path="/staffTimeSheets" element={<StaffTimeTable />} />
+          <Route path="/file-data-reader" element={<FileDataReader />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
