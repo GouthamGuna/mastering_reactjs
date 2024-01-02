c14ddf4706acd8dcbd9256066fe1e00ac84ea1fa
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-02 21:47:24 +0530

    initial commit

---
+++ reference_work_api_front-end/src/components/FileUpload.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//import Navbar from './Navbar'
+
+function FileUpload() {
+  return (
+    <div>
+        {/* <Navbar /> */}
+        <h1>File Upload</h1>
+    </div>
+  )
+}
+
+export default FileUpload
