13bfc46503e49cd6d49b1e0b285ee93aae7cbfa1
GouthamGuna <ggowthamsankar@gmail.com>
2024-02-18 15:47:02 +0530

    Udate App.js

--- course/src/App.js
+++ course/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Accordion from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image_slider";
 
 function App() {
   return (
@@ -9,6 +10,11 @@ function App() {
       <Accordion />
       <RandomColor />
       <StarRating noOfStars={10} />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={1}
+        //  limit={} options
+      />
     </div>
   );
 }
